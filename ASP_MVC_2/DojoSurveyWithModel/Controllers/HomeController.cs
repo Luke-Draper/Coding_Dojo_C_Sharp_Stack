a6d19664f848ae4a7bc29240ac3c5a9a86661038
Luke Draper <lukepdraper@gmail.com>
2019-01-29 15:41:41 -0600

    Complete Dojo Survey With Model

---
+++ ASP_MVC_2/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("")]
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Route("result")]
+		public ViewResult Result(Survey input)
+		{
+			ViewBag.Survey = input;
+			return View();
+		}
+
+	}
+}
---
+++ ASP_MVC_2/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DojoSurveyWithModel.Models
+{
+	public class Survey
+	{
+		public string Name {get;set;}
+		public string Location {get;set;}
+		public string FavoriteLanguage {get;set;}
+		public string Comment {get;set;}
+	}
+}
