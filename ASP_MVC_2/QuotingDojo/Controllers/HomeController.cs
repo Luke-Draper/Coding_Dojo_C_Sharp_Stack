74601ec2e8f52fcaceaa52cdac2cdaa5c064b109
Luke Draper <lukepdraper@gmail.com>
2019-02-04 00:31:20 -0600

    Completed Quoting Dojo

---
+++ ASP_MVC_2/QuotingDojo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+			Random rnd = null;
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+				// remember to use [HttpPost] attributes!
+				[HttpPost]
+				[Route("quotes")]
+				public IActionResult Method(string Name, string Content)
+				{
+					Quotes new_quote = new Quotes {
+						Name = Name,
+						Content = Content,
+						Created_At = DateTime.Now,
+					};
+					if (rnd == null)
+					{
+						rnd = new Random();
+					}
+					int id = rnd.Next(1);
+
+					string query = $"INSERT INTO quotes (quote_id, name, content, created_at) VALUES ({id}, '{new_quote.Name}', '{new_quote.Content}', '{new_quote.Created_At.ToString("yyyy-MM-dd HH:mm:ss")}')";
+					DbConnector.Execute(query);
+
+					return RedirectToAction("Quotes");
+				}
+
+				[Route("quotes")]
+        [HttpGet]
+        public IActionResult Quotes()
+        {
+					
+    	    	List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes");
+            ViewBag.Quotes = AllQuotes;
+            return View();
+        }
+
+    }
+}
---
+++ ASP_MVC_2/QuotingDojo/Models/Quotes.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuotingDojo.Models
+{
+	public class Quotes
+	{
+		public string Name {set;get;}
+		public string Content {set;get;}
+		public DateTime Created_At {set;get;}
+	}
+}
